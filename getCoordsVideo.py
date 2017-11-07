53be03ffcdf2ebe40f9761a5ef7f53b64d78b835
Hayley Moore <hayley@orange>
2017-11-07 17:12:52 +0000

    update

--- getCoordsVideo.py
+++ getCoordsVideo.py
@@ -5,40 +5,77 @@ import scipy.ndimage as ndimage
 from imutils import contours
 import imutils as im
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
 
 
-def track(videoLocation, plot, darkTolerance, sizeOfObject, radi, lowerBoundY = 0, upperBoundY = 2500, lowerBoundX = 0, upperBoundX = 3000):
+def track(videoLocation, plot, darkTolerance, sizeOfObject, radi, test = False, lowerBoundY = 0, upperBoundY = 2500, lowerBoundX = 0, upperBoundX = 3000):
+    test_xmin = 600
+    test_xmax = 800
+    test_ymin = 1400
+    test_ymax = 1200
+
+
     sheepLocations = []
     frameID = 0
 
     cap = cv2.VideoCapture(videoLocation)
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     print length
-    while(frameID <= 0):
+    while(frameID < 700):
+        ret, frame = cap.read()
+        print frameID
+        frameID +=1
+    while(frameID <= 700):
         ret, frame = cap.read()
         if ret == True:
 
-            print frameID
+
 
             full = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-            #plt.imshow(full)
-            #plt.pause(5)
+            if test == True:
+                plt.imshow(full)
+                plt.xlim(xmin = test_xmin, xmax = test_xmax)
+                plt.ylim(ymax = test_ymax, ymin = test_ymin)
+                id = 1
+                plt.savefig('./test'+str(id)+'.png')
 
             grey = full[:,:,2]#cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
             grey[grey < darkTolerance] = 0.0
             grey[grey > 170.] = 255.
-            #plt.imshow(grey)
-            #plt.pause(5)
+            if test == True:
+                plt.imshow(grey)
+                plt.xlim(xmin = test_xmin, xmax = test_xmax)
+                plt.ylim(ymax = test_ymax, ymin = test_ymin)
+                id += 1
+                plt.savefig('./test'+str(id)+'.png')
 
             img = ndimage.gaussian_filter(grey, sigma = (2), order = 0)
-            #plt.imshow(img)
-            #plt.pause(5)
+            if test == True:
+                plt.imshow(img)
+                plt.xlim(xmin = test_xmin, xmax = test_xmax)
+                plt.ylim(ymax = test_ymax, ymin = test_ymin)
+                id += 1
+                plt.savefig('./test'+str(id)+'.png')
 
-            filtered = ndimage.maximum_filter(img, size=2)
+            maxfilter = ndimage.maximum_filter(img, size=2)
+            if test == True:
+                plt.imshow(maxfilter)
+                plt.xlim(xmin = test_xmin, xmax = test_xmax)
+                plt.ylim(ymax = test_ymax, ymin = test_ymin)
+                id += 1
+                plt.savefig('./test'+str(id)+'.png')
 
+            filtered = maxfilter
             filtered[filtered < 65.] = 0.0 #for removing extra shiney grass
             filtered[filtered > 0.] = 255.
+            if test == True:
+                plt.imshow(filtered)
+                plt.xlim(xmin = test_xmin, xmax = test_xmax)
+                plt.ylim(ymax = test_ymax, ymin = test_ymin)
+                id += 1
+                plt.savefig('./test'+str(id)+'.png')
 
             labels = measure.label(filtered, neighbors=8, background=0)
 
@@ -56,6 +93,7 @@ def track(videoLocation, plot, darkTolerance, sizeOfObject, radi, lowerBoundY = 
                 # number of pixels
                 labelMask = np.zeros(filtered.shape, dtype="uint8")
                 labelMask[labels == label] = 1
+
                 numPixels = (labelMask > 0).sum()
                 # if the number of pixels in the component is sufficiently
                 # large, then add it to our mask of "large blobs"
@@ -63,7 +101,6 @@ def track(videoLocation, plot, darkTolerance, sizeOfObject, radi, lowerBoundY = 
                 if numPixels > sizeOfObject:
                     cnts = cv2.findContours(labelMask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[1][0]
                     ((cX, cY), radius) = cv2.minEnclosingCircle(cnts)
-
                     if cY < lowerBoundY:
                         radius = 0
                     if cX < lowerBoundX:
@@ -81,24 +118,54 @@ def track(videoLocation, plot, darkTolerance, sizeOfObject, radi, lowerBoundY = 
                             if numPixels < 250:
                                 objectLocations = objectLocations + [[cX, cY]]
                             elif numPixels < 450:
-                                ymin = np.min(np.where(labelMask[:,1] == 1))
-                                ymax = np.max(np.where(labelMask[:,1] == 1))
-                                print ymin, ymax
-                                objectLocations = objectLocations + [[cX-radius/2., cY-radius/2.], [cX+radius/2., cY+radius/2.]]
+                                av_score = []
+                                thresh = 210
+                                x,y,w,h = cv2.boundingRect(cnts)
+                                maxfilter = ndimage.maximum_filter(img, size=2)
+                                maxfilter[maxfilter < thresh] = 0
+
+                                miniImg = maxfilter[y: y + h, x: x + w]
+                                for n_clusters in np.array([2,3,4]):
+                                    clusterer = KMeans(n_clusters=n_clusters, random_state=10)
+
+                                    cluster_labels = clusterer.fit_predict(np.transpose(np.where(miniImg > thresh)))
+
+                                    av_score += [silhouette_score(np.transpose(np.where(miniImg > thresh)), cluster_labels)]
+                                print np.where(av_score == np.max(av_score))[0][0] + 2
+                                plt.imshow(maxfilter)
+                                plt.colorbar()
+                                plt.ylim(ymin=y+h-1, ymax=y)
+                                plt.xlim(xmin=x, xmax=x+w-1)
+                                plt.show()
+                                plt.clf()
+                                objectLocations = objectLocations + [[cX-1, cY-1],[cX+1,cY+1]]
+                            elif numPixels < 650:
+                                objectLocations = objectLocations + [[cX-1, cY-1],[cX+1,cY+1],[cX-1, cY+1]]
                             else:
-                                objectLocations = objectLocations + [[cX-radius/2., cY-radius/2.], [cX+radius/2., cY+radius/2.], [cX-radius/2, cY+radius/2]]
+                                approxNumber = np.ceil((numPixels - 50.)/200.)
+                                print 'shifts', approxNumber
+                                shifts = np.linspace(-1,1,approxNumber)
+                                objectLocations = objectLocations + np.transpose(np.array([cX - shifts, cY + shifts])).tolist()
 
 
-            print np.shape(objectLocations)[0]
             sheepLocations = sheepLocations + [objectLocations]
-
-            if plot != 0:
+            print frameID, len(objectLocations)
+            if plot != 'N':
                 plt.cla()
-                plt.imshow(filtered)
+                plt.imshow(full)
                 plt.scatter(np.array(objectLocations)[:, 0], np.array(objectLocations)[:, 1], s = 6.)
-                plt.xlim(xmin = 1000, xmax = 2000)
-                plt.ylim(ymax = 1000)
-                plt.pause(15)
+                if test == True:
+                    plt.xlim(xmin = test_xmin, xmax = test_xmax)
+                    plt.ylim(ymax = test_ymax, ymin = test_ymin)
+                    id += 1
+                else:
+                    plt.xlim(xmin = 0, xmax = 2500)
+                    plt.ylim(ymax = 0, ymin = 2000)
+                #plt.axis('equal')
+                if plot == 's':
+                    plt.savefig('/Users/hayleymoore/Documents/PhD/Tracking/throughFenceRL/'+str(frameID).zfill(4))
+                else:
+                    plt.pause(15)
 
             frameID += 1
 
@@ -106,5 +173,5 @@ def track(videoLocation, plot, darkTolerance, sizeOfObject, radi, lowerBoundY = 
     plt.clf()
     return [np.array(sheepLocations), r, pix]
 
-locations, radii, pixels = track('/Users/hayleymoore/Documents/PhD/Tracking/throughFenceRL.mp4', plot = 1, darkTolerance = 172.5, sizeOfObject = 60, radi = 5., upperBoundX = 2000, lowerBoundY = 900, lowerBoundX = 100)
-print locations
+locations, radii, pixels = track('/Users/hayleymoore/Documents/PhD/Tracking/throughFenceRL.mp4', plot = 'Y', test = False, darkTolerance = 173.5, sizeOfObject = 60, radi = 5., upperBoundX = 2000, lowerBoundY = 500, lowerBoundX = 100)
+np.save('locfull.npy',locations)
--- getTraj.py
+++ getTraj.py
@@ -2,21 +2,36 @@ import numpy as np
 from sklearn.neighbors import KDTree
 import matplotlib.pyplot as plt
 
-locations = np.load('loc.npy')
+locations = np.load('loc100.npy')
 
 print locations
 
-for i in range(40):
+for i in range(99):
     print np.shape(locations[i])
 
-traj = [locations[0]]
+traj = []
 
-print traj
-t = []
 
 for time in range(1):#range(len(locations)):
-    if np.shape(locations[time]) < np.shape(locations[time+1]):
+    t = []
+    t_end = []
+    if np.shape(locations[time]) == np.shape(locations[time+1]):
+        print 'every element in 0 has a partner in 1'
+        tree = KDTree(locations[time+1])
+        dist, idx = tree.query(locations[time], 1)
+        for i in range(len(dist)):
+            if dist[i] < 50:
+                t += [locations[time]]
+    elif np.shape(locations[time]) < np.shape(locations[time+1]):
         print 'every element in 0 needs a partner in 1'
+        tree = KDTree(locations[time+1])
+        dist, idx = tree.query(locations[time], 1)
+        for i in range(len(dist)):
+            if dist[i] < 50:
+                t += [locations[time]]
+
+    else:
+        print 'every element in 1 needs a partner in 0'
         tree = KDTree(locations[time+1])
         dist, idx = tree.query(locations[time], 1)
         for i in range(len(dist)):
@@ -24,22 +39,20 @@ for time in range(1):#range(len(locations)):
                 t += [locations[time+1][idx[i][0]]]
             else:
                 t += [[np.nan, np.nan]]
-    else:
-        print 'every element in 1 needs a partner in 0'
-        tree = KDTree(locations[time])
-        dist, idx = tree.query(locations[time+1], 1)
-        for i in range(len(dist)):
-            if dist[i] < 1000000:
-                t += [locations[time+1][idx[i][0]]]
-            else:
-                t += [[np.nan, np.nan]]
 
-    traj += [t]
+    if np.shape(traj)[1] == np.shape(t+t_end)[0]:
+        traj += [t + t_end]
+    elif np.shape(traj)[1] < np.shape(t+t_end)[0]:
+        traj += [t + t_end]
+    elif np.shape(traj)[1] > np.shape(t+t_end)[0]:
+        traj += [t + t_end]
+    else:
+        break
 
 traj = np.array(traj)
+plt.scatter(np.array(locations[0])[:,0], np.array(locations[0])[:,1], alpha = 0.5, marker='+')
 plt.scatter(np.array(locations[1])[:,0], np.array(locations[1])[:,1], alpha = 0.5)
 for i in range(np.shape(traj)[1]):
     plt.plot(traj[:,i,0], traj[:,i,1], color = 'k')
-plt.scatter(traj[1,:,0], traj[1,:,1], marker = 'x')
-
+plt.ylim(ymax=800,ymin=2000)
 plt.show()
