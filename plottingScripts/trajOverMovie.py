d5cb344804a6cfe034f9b3aca234c2239c0ff1c7
Hayley Moore <h.e.l.moore@newcastle.ac.uk>
2018-05-15 11:52:15 +0100

    now has colourful fading lines

--- plottingScripts/trajOverMovie.py
+++ plottingScripts/trajOverMovie.py
@@ -5,6 +5,10 @@ from skimage import measure
 import scipy.ndimage as ndimage
 from imutils import contours
 import imutils as im
+from matplotlib import cm
+import matplotlib as mpl
+mpl.use('Agg')
+
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_samples, silhouette_score
@@ -36,7 +40,7 @@ restart = 50
 sheepLocations = []
 sheepVelocity = []
 frameID = 0
-cropVector = [1000,1000,2000,2028]
+cropInit = [1000,1000,2000,2028]
 
 
 
@@ -45,27 +49,43 @@ cap = cv2.VideoCapture(videoLocation)
 length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 print 'You have', length, 'frames', videoLocation
 
-sheep = np.load('locfull.npy')
+if (videoLocation.rfind('data') > 0) and (videoLocation.rfind('throughFenceRL') > 0):
+    print 'Skipping first 15 frames'
+    while(frameID <= 15):
+        ret, frame = cap.read()
+        frameID += 1
+
+    if frameID > 0:
+        frameID = 0
+
+sheep = np.load(save + 'loc200.npy')[0]
 sheep =  map(np.array,  sheep)
 print 'You have analysed', len(sheep), 'frames'
 
 S = 0
-F = len(sheep)
+F = 200#len(sheep)
 
-lenTraj = 10
+lenTraj = 75
 movingAverage = 6
 
 N = len(sheep[0])
+alph = np.linspace(0,1,F)
+colors = [ cm.prism(x) for x in np.linspace(0., 1., N)]
 
-while(frameID <= len(sheep)-6):
+
+while(frameID <= len(sheep)-movingAverage):
     ret, frame = cap.read()
     print frameID
     full = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    fullCropped, cropVector = movingCrop(frameID, full, sheep, cropVector)
+    if frameID == 0:
+        cropVector = cropInit
+    _, cropVector = movingCrop(frameID, full, sheep, cropVector)
+    fullCropped = full[min(cropInit[1], cropVector[1]):cropInit[3], min(cropInit[0],cropVector[0]):cropInit[2],:]
     cropX, cropY, _,  _ = cropVector
     plt.clf()
     plt.imshow(fullCropped)
     for j in range(N):
+
     	smoothX = np.convolve(np.array(sheep)[S:F,j,0], np.ones((movingAverage,))/movingAverage, mode='valid')
     	smoothY = np.convolve(np.array(sheep)[S:F,j,1], np.ones((movingAverage,))/movingAverage, mode='valid')
     	for i in range(3):
@@ -73,10 +93,16 @@ while(frameID <= len(sheep)-6):
     		smoothY =  np.append([smoothY[0]],  smoothY)
     		smoothX =  np.append(smoothX, [smoothX[-1]])
     		smoothY =  np.append(smoothY, [smoothY[-1]])
-    	plt.plot(smoothX[max(frameID-lenTraj,  0):frameID]-cropX, smoothY[max(frameID-lenTraj,  0):frameID]-cropY, lw = 1, color='blue')
+        pltX = smoothX[max(frameID-lenTraj,  0):frameID]-cropX
+        pltY = smoothY[max(frameID-lenTraj,  0):frameID]-cropY
+        L = len(pltX)
+        c = np.abs(colors[j])
+        c = np.tile(c, L).reshape(L,4)
+        c[:,3] = np.exp(5*np.linspace(0.01,1,L))/np.exp(5)
+    	plt.scatter(pltX, pltY,s=0.2, color=c)
     	plt.gca().set_aspect('equal')
     plt.gca().set_axis_off()
-    plt.savefig(save+'traj'+str(frameID).zfill(4), bbox_inches='tight',  dpi = 300)
+    plt.savefig(save+'trajMovie/traj'+str(frameID).zfill(4)+'.pdf', format='pdf', bbox_inches='tight',  dpi = 300)
 
 
 
