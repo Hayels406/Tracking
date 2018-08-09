219368a0d4005f35fdfc257aec92edf47e857f01
Hayley Moore <hayley@orange>
2018-08-09 13:40:37 +0100

    sweep complete

--- plottingScripts/paramSweep_gauss.py
+++ plottingScripts/paramSweep_gauss.py
@@ -9,17 +9,17 @@ save = '/data/b1033128/Tracking/CaseH2/'
 plt.close('all')
 plt.ion()
 l = [3,5,7,9,11,13]
-data = np.zeros([6, len(np.arange(0.5, 10.5, 0.5))])
+data = np.zeros([6, len(np.arange(1., 10, 0.5))])
 for lG in l:
     i_l = np.where(np.array(l) == lG)[0][0]
     print i_l
-    for sigmaG in np.arange(0.5, 10.5, 0.5):
-        i_s  = np.where(np.arange(0.5, 10.5, 0.5) == sigmaG)[0][0]
+    for sigmaG in np.arange(1., 10, 0.5):
+        i_s  = np.where(np.arange(1, 10, 0.5) == sigmaG)[0][0]
 
         #setting up file directories
-        sig = str(round(sigmaG,3)*100)
-        l1 = sig[:1]
-        l2 = sig[2:]
+        sig = str(round(sigmaG,2)*10)
+        l1 = sig[0]
+        l2 = sig[1:]
 
         file = save+'ParamSweep/gaussianL'+str(lG)+'/sigma' +l1+'-'+l2+'/sheepDetected.csv'
         if len(glob(file)) > 0:
@@ -32,11 +32,11 @@ plt.imshow(data == 44)
 ax = plt.gca()
 ax.invert_yaxis()
 ax.set_yticklabels(['', '3', '5', '7', '9', '11', '13'])
-ax.set_xticklabels(['', '0.5', '1.5', '2.5', '3.5', '4.5', '5.5', '6.5', '7.5'])
+plt.xticks(np.arange(0, 18, 1.), np.arange(1,10,0.5))
 plt.xlabel('$\sigma_G$')
-plt.ylabel('Number Dectected')
+plt.ylabel('$\ell_G$')
 plt.savefig(save+'ParamSweep/gaussParamSweep.pdf', format='pdf')
-np.savetxt(save+'pgfData/gaussIdentifies.txt', np.append(np.append(np.array(sorted(l*20)).reshape(20*6, 1), np.tile(np.arange(0.5, 10.5, 0.5), 6).reshape(20*6,1), axis = 1), (data == 44).flatten().reshape(20*6,1), axis = 1), fmt='%1.1e')
+np.savetxt(save+'pgfData/gaussIdentifies.txt', np.append(np.append(np.array(sorted(l*18)).reshape(18*6, 1), np.tile(np.arange(1, 10, 0.5), 6).reshape(18*6,1), axis = 1), (data == 44).flatten().reshape(18*6,1), axis = 1), fmt='%1.1e')
 
 plt.close('all')
 plt.plot(l,(data==44).sum(axis=1)*1./np.shape(data)[1])
