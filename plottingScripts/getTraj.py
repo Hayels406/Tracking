97d6a8c509f6c338cd22f7e1ec1615e9907d7d81
Hayley Moore <hayley@orange>
2018-08-09 13:41:49 +0100

    can now plot all extracted videos

--- plottingScripts/getTraj.py
+++ plottingScripts/getTraj.py
@@ -6,16 +6,29 @@ from matplotlib import rc
 from glob import glob
 import os
 
+plt.close('all')
+
 video = sys.argv[1]
 videoLocation = '/data/b1033128/Videos/'+video
 save = '/data/b1033128/Tracking/'+video[:-4] +'/'
+ymax, xmax = (1520, 2704)
 
-
+if (videoLocation.rfind('CaseH2') > 0) or (videoLocation.rfind('CaseJ2') > 0) or (videoLocation.rfind('CaseH3') > 0):
+    video = 'video2'
+if (video[:-4]=='CaseJ'):
+    save = '/data/b1033128/Tracking/'+video[:-4] +'1/'
+    video = 'CJ1'
+if (video[:-4]=='CaseH'):
+    save = '/data/b1033128/Tracking/'+video[:-4] +'1/'
+    video = 'CH1'
 
 data = glob(save+'Final-loc*')[-1]
 sheep = np.load(data)
 quad = np.load(glob(save+'Final-quad*')[-1])
 sheep =  map(np.array,  sheep)
+if video == 'video2':
+    blackSheepData = glob(save+'Final-blackSheep*')[-1]
+    blackSheep = np.load(blackSheepData)
 print 'You have analysed', len(sheep), 'frames'
 
 
@@ -31,12 +44,111 @@ N = np.shape(sheep)[1]
 S = 0
 F = len(sheep)
 
-colors = [ cm.prism(x) for x in np.linspace(0., 1., N)]
+if video == 'video2':
+    corners = np.array([[2704,  463],
+                        [2174,  517],
+                        [2063,  371],
+                        [1952,  364],
+                        [1717,  374],
+                        [1389,  390],
+                        [1399,  566],
+                        [1389,  390],
+                        [1098,  401],
+                        [903,   405],
+                        [903,   349],
+                        [723,   357],
+                        [600,   363],
+                        [600,   288],
+                        [89,    307],
+                        [11,    1502],
+                        [494,   1439],
+                        [894,   1485],
+                        [1182,  1442],
+                        [1275,  1419],
+                        [1462,  1387],
+                        [1421,  893],
+                        [1462,  1387],
+                        [1968,  1293],
+                        [2036,  1515],
+                        [1411,  731],
+                        [1418,  826]  ])
+    corners = np.abs(corners - [0, ymax])
+    ax = plt.gca()
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([1838, 369]) - [0,ymax])), 110, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([ 903, 396]) - [0,ymax])), 190, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([ 377, 290]) - [0,ymax])), 45, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([ 200, 450]) - [0,ymax])), 120, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([ 133, 665]) - [0,ymax])), 50, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([ 196, 890]) - [0,ymax])), 85, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([ 110,1100]) - [0,ymax])), 125, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([ 173,1291]) - [0,ymax])), 80, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([ 418,1485]) - [0,ymax])), 65, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([ 795,1155]) - [0,ymax])), 125, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([ 795,1440]) - [0,ymax])), 70, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([1075,1450]) - [0,ymax])), 40, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([1416,1378]) - [0,ymax])), 50, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([2150,1290]) - [0,ymax])), 105, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([2529,1335]) - [0,ymax])), 220, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([2240,1455]) - [0,ymax])), 165, color='g'))
+
+    ax.plot(corners[:-2,0], corners[:-2,1], color='k')
+    ax.plot(corners[-2:,0], corners[-2:,1], color='k')
+elif video == 'CJ1':
+    ymax, xmax, _ = (1520, 2704, 3)
+    fence1 = np.array([ [289,  0],
+                        [406,  1125],
+                        [265,  1365],
+                        [114,  ymax],
+                        [265,  1365],
+                        [389,  ymax]  ])
+
+    fence2 = np.array([ [2349,  0],
+                        [2225,  1030],
+                        [1975,  ymax]  ])
+    fence1 = np.abs(fence1 - [0, ymax])
+    fence2 = np.abs(fence2 - [0, ymax])
+
+    ax = plt.gca()
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([307, 143]) - [0,ymax])), 64, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([330, 460]) - [0,ymax])), 64, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([396, 788]) - [0,ymax])), 39, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([355,1178]) - [0,ymax])), 39, color='g'))
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([232,1510]) - [0,ymax])), 124, color='g'))
+
+    ax.plot(fence1[:-2,0], fence1[:-2,1], color='k')
+    ax.plot(fence1[-2:,0], fence1[-2:,1], color='k')
+    ax.plot(fence2[:,0], fence2[:,1], color='k')
+
+elif video == 'CH1':
+    ymax, xmax, _ = (1520, 2704, 3)
+    fence1 = np.array([ [0,     1383],
+                        [2517,  1147],
+                        [xmax,  1082]  ])
+    fence1 = np.abs(fence1 - [0, ymax])
+
+    ax = plt.gca()
+    ax.add_artist(plt.Circle(tuple(np.abs(np.array([730,152]) - [0,ymax])), 110, color='g'))
+    ax.plot(fence1[:,0], fence1[:,1], color='k')
+
 for j in range(N):
-    plt.plot(np.convolve(sheep[S:F,j,0], np.ones((5,))/5, mode='valid'), np.convolve(sheep[S:F,j,1], np.ones((5,))/5, mode='valid'), color = colors[j], lw = 1)
+    plt.plot(np.convolve(sheep[S:F,j,0], np.ones((5,))/5, mode='valid'), np.abs(np.convolve(sheep[S:F,j,1], np.ones((5,))/5, mode='valid') - ymax), color = 'blue', lw = 2, alpha = 0.5)
+plt.scatter(sheep[0,:,0], np.abs(sheep[0,:,1]-ymax), color='green', alpha = 0.7)
+plt.scatter(sheep[-1,:,0], np.abs(sheep[-1,:,1]-ymax), color='red', alpha = 0.7)
+
+if video == 'video2':
+    plt.plot(np.convolve(blackSheep[S:F,0], np.ones((5,))/5, mode='valid'), np.abs(np.convolve(blackSheep[S:F,1], np.ones((5,))/5, mode='valid') - ymax), color = 'blue', lw = 2, alpha = 0.5)
+    plt.scatter(blackSheep[0,0], np.abs(blackSheep[0,1]-ymax), color='green', alpha = 0.7)
+    plt.scatter(blackSheep[-1,0], np.abs(blackSheep[-1,1]-ymax), color='red', alpha = 0.7)
+
+
+plt.plot(np.convolve(quad[S:F,0], np.ones((5,))/5, mode='valid'), np.abs(np.convolve(quad[S:F,1], np.ones((5,))/5, mode='valid') - ymax), color = 'red', lw = 2)
+plt.scatter(quad[0,0], np.abs(quad[0,1]-ymax), color='green', alpha = 0.7)
+plt.scatter(quad[-1,0], np.abs(quad[-1,1]-ymax), color='red', alpha = 0.7)
 
 plt.axes().set_aspect('equal')
-plt.gca().invert_yaxis()
 for label in plt.gca().get_xticklabels() + plt.gca().get_yticklabels():
     label.set_fontsize(12)
-plt.savefig(save+'traj'+str(len(sheep) - 1)+'.pdf', format='pdf')
+plt.xlim(0, 2704)
+plt.ylim(0, 1520)
+plt.savefig(save+'traj'+str(len(sheep) - 1)+'.pdf', format='pdf', dpi = 300)
+os.system('pdfcrop ' +save+'traj'+str(len(sheep) - 1)+'.pdf '+save+'traj'+str(len(sheep) - 1)+'.pdf')
--- plottingScripts/trajOverMovie.py
+++ plottingScripts/trajOverMovie.py
@@ -26,15 +26,7 @@ videoLocation = '/data/b1033128/Videos/'+video
 save = '/data/b1033128/Tracking/'+video[:-4] +'/'
 print save
 
-plot = 's'
-darkTolerance = 173.5
-sizeOfObject = 60
-restart = 50
-
-sheepLocations = []
-sheepVelocity = []
 frameID = 0
-cropInit = [1000,1000,2000,2028]
 
 
 
@@ -43,23 +35,28 @@ cap = cv2.VideoCapture(videoLocation)
 length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 print 'You have', length, 'frames', videoLocation
 
-if (videoLocation.rfind('data') > 0) and (videoLocation.rfind('throughFenceRL') > 0):
-    print 'Skipping first 15 frames'
-    while(frameID <= 15):
-        ret, frame = cap.read()
-        frameID += 1
-
-    if frameID > 0:
-        frameID = 0
-
 if (videoLocation.rfind('data') > 0) and (videoLocation.rfind('CaseJ2') > 0):
+    blackSheepID = True
     print 'Skipping first 10 frames'
     while(frameID <= 10):
         ret, frame = cap.read()
         frameID += 1
+
 if (videoLocation.rfind('data') > 0) and (videoLocation.rfind('CaseH2') > 0):
+    blackSheepID = True
     print 'Skipping first 19 frames'
     while(frameID <= 19):
+        ret, frame = cap.read()
+        frameID += 1
+
+if (videoLocation.rfind('data') > 0) and (videoLocation.rfind('CaseH3') > 0):
+    blackSheepID = True
+
+if (videoLocation.rfind('data') > 0) and (videoLocation.rfind('CaseJ') > 0):
+    blackSheepID = False
+    save = '/data/b1033128/Tracking/'+video[:-4] +'1/'
+    print 'Skipping first 15 frames'
+    while(frameID <= 15):
         ret, frame = cap.read()
         frameID += 1
 
@@ -67,6 +64,9 @@ if frameID > 0:
     frameID = 0
 data = glob(save+'Final-loc*')[-1]
 sheep = np.load(data)
+if blackSheepID:
+    blackSheepData = glob(save+'Final-blackSheep*')[-1]
+    blackSheep = np.load(blackSheepData)
 quad = np.load(glob(save+'Final-quad*')[-1])
 sheep =  map(np.array,  sheep)
 print 'You have analysed', len(sheep), 'frames'
@@ -86,15 +86,9 @@ while(frameID <= len(sheep)-movingAverage):
     ret, frame = cap.read()
     print frameID
     full = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-    if frameID == 0:
-        cropVector = cropInit
-    _, cropVector = movingCrop(frameID, full, sheep, cropVector)
-    fullCropped = full[min(cropInit[1], cropVector[1]):cropInit[3], min(cropInit[0],cropVector[0]):cropInit[2],:]
-    cropX, cropY, _,  _ = cropVector
     plt.clf()
     plt.imshow(full)
     for j in range(N):
-
     	smoothX = np.convolve(np.array(sheep)[S:F,j,0], np.ones((movingAverage,))/movingAverage, mode='valid')
     	smoothY = np.convolve(np.array(sheep)[S:F,j,1], np.ones((movingAverage,))/movingAverage, mode='valid')
     	for i in range(3):
@@ -111,6 +105,10 @@ while(frameID <= len(sheep)-movingAverage):
     	plt.scatter(pltX, pltY,s=0.2, color=c)
     	plt.gca().set_aspect('equal')
 
+    delay = 0
+    if (video[:-4] == 'CaseJ') and (frameID > 80):
+        delay = 80
+    
     smoothX = np.convolve(np.array(quad)[S:F,0], np.ones((movingAverage,))/movingAverage, mode='valid')
     smoothY = np.convolve(np.array(quad)[S:F,1], np.ones((movingAverage,))/movingAverage, mode='valid')
     for i in range(3):
@@ -118,17 +116,38 @@ while(frameID <= len(sheep)-movingAverage):
         smoothY =  np.append([smoothY[0]],  smoothY)
         smoothX =  np.append(smoothX, [smoothX[-1]])
         smoothY =  np.append(smoothY, [smoothY[-1]])
-    pltX = smoothX[max(frameID-lenTraj,  0):frameID]
-    pltY = smoothY[max(frameID-lenTraj,  0):frameID]
+    pltX = smoothX[max(frameID-lenTraj-delay,  0):frameID-delay]
+    pltY = smoothY[max(frameID-lenTraj-delay,  0):frameID-delay]
     L = len(pltX)
     c = (0., 0., 0., 1.0)
     c = np.tile(c, L).reshape(L,4)
     c[:,3] = np.exp(5*np.linspace(0.01,1,L))/np.exp(5)
-    plt.scatter(pltX, pltY,s=0.2, color=c)
+    if (video[:-4] == 'CaseJ') and (frameID > 80):
+        plt.scatter(pltX, pltY, s=0.2, color=c)
+    elif video[:-4] != 'CaseJ':
+        plt.scatter(pltX, pltY, s=0.2, color=c)
+
+    if blackSheepID:
+        smoothX = np.convolve(np.array(blackSheep)[S:F,0], np.ones((movingAverage,))/movingAverage, mode='valid')
+        smoothY = np.convolve(np.array(blackSheep)[S:F,1], np.ones((movingAverage,))/movingAverage, mode='valid')
+        for i in range(3):
+            smoothX =  np.append([smoothX[0]],  smoothX)
+            smoothY =  np.append([smoothY[0]],  smoothY)
+            smoothX =  np.append(smoothX, [smoothX[-1]])
+            smoothY =  np.append(smoothY, [smoothY[-1]])
+        pltX = smoothX[max(frameID-lenTraj,  0):frameID]
+        pltY = smoothY[max(frameID-lenTraj,  0):frameID]
+        L = len(pltX)
+        c = np.abs(colors[0])
+        c = np.tile(c, L).reshape(L,4)
+        c[:,3] = np.exp(5*np.linspace(0.01,1,L))/np.exp(5)
+        plt.scatter(pltX, pltY,s=0.2, color=c)
+
     plt.gca().set_axis_off()
     plt.savefig(save+'trajMovie/traj'+str(frameID).zfill(4)+'.png', format='png', bbox_inches='tight',  dpi = 300)
 
-
-
-
     frameID +=1
+
+
+os.system('for a in '+save+'trajMovie/*.png; do convert -trim "$a" "$a"; done')
+os.system('ffmpeg -framerate 25 -pattern_type glob -i "'+save+'trajMovie/traj0*.png" -c:v libx264 -r 30 -pix_fmt yuv420p -vf scale=1200:1180 "'+save+'trajMovie.mp4"')
