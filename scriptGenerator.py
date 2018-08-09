c82908147f1084937b80943725001f870f4125aa
Hayley Moore <hayley@orange>
2018-08-09 13:43:05 +0100

    minor chages

--- scriptGenerator.py
+++ scriptGenerator.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-script = 'paramSweepVideo_max_check.py '
+script = 'paramSweepVideo_gaussian_check.py '
 
-for lG in [1,3,5,7,9]:
-    print 'python ' + script + str(lG)
+for lG in [3,5,7,9,11,13]:
+    for sigma in np.arange(1, 10, 0.5):
+        print 'python ' + script + str(lG) + ' ' + str(sigma)
